7ccee81434f384d77f7e5ab13c56c5cf18f462e0
noanswerss <ciprianvlad888@gmail.com>
2024-01-21 12:04:31 +0000

    moved files in proper folders

--- script.js
+++ script.js
@@ -1,86 +1,89 @@
 "use strict";
 
-window.addEventListener( "load", function() {
+document.addEventListener("DOMContentLoaded", function () {
 
-// Variables
-const faqCard = document.querySelector( ".faq-card" );
-const faqList = document.querySelector( ".faq-list" );
-const faqItems = [ ...document.querySelectorAll( ".faq-item" ) ];
-const faqItemHeaders = [ ...document.querySelectorAll( ".faq-item-header" ) ];
+  // Variables
+  const faqsCard = document.querySelector(".faqs-card");
+  const faqsList = document.querySelector(".faqs-list");
+  const faqsItems = document.querySelectorAll(".faqs-item");
+  const faqsItemHeaders = document.querySelectorAll(".faqs-item-header");
 
-// Functions
-const displayActiveIcon = function( activeIcon, inactiveIcon ) {
-  activeIcon.style.display = "block";
-  inactiveIcon.style.display = "none";
-}
+  // Functions
+  const displayActiveIcon = (activeIcon, inactiveIcon) => {
+    activeIcon.style.display = "block";
+    inactiveIcon.style.display = "none";
+  };
 
-const closeFaqItems = function() {
-  faqItems.forEach( faqItem => {
-    const closeHeight = faqItem.querySelector( ".faq-item-header" ).scrollHeight;
-    faqItem.style.maxHeight = `${ closeHeight }px`;
-    faqItem.dataset.openFaqList = false;
-    displayActiveIcon(
-      faqItem.querySelector( ".plus-icon" ),
-      faqItem.querySelector( ".minus-icon" ) );
-  } );
-}
-
-closeFaqItems();
-
-// Faq Item Click Event
-faqList.addEventListener( "click", function( event ) {
-  if( event.target.closest( ".faq-item-header" ) ) {
-    const targetFaqItem = event.target.closest( ".faq-item-header" ).closest( ".faq-item" );
-    if( targetFaqItem.dataset.openFaqList === "false" ) {
-      closeFaqItems();
-      targetFaqItem.style.maxHeight = `${ targetFaqItem.scrollHeight }px`;
-      targetFaqItem.dataset.openFaqList = true;
+  const closefaqsItems = () => {
+    faqsItems.forEach((faqsItem) => {
+      const closeHeight = faqsItem.querySelector(".faqs-item-header").scrollHeight;
+      faqsItem.style.maxHeight = `${closeHeight}px`;
+      faqsItem.dataset.openfaqsList = false;
       displayActiveIcon(
-        targetFaqItem.querySelector( ".minus-icon" ),
-        targetFaqItem.querySelector( ".plus-icon" )
+        faqsItem.querySelector(".plus-icon"),
+        faqsItem.querySelector(".minus-icon")
       );
-    } else {
-      closeFaqItems();
+    });
+  };
+
+  closefaqsItems();
+
+  // faqs Item Click Event
+  faqsList.addEventListener("click", (event) => {
+    const faqsItemHeader = event.target.closest(".faqs-item-header");
+    if (faqsItemHeader) {
+      const targetfaqsItem = faqsItemHeader.closest(".faqs-item");
+      if (targetfaqsItem.dataset.openfaqsList === "false") {
+        closefaqsItems();
+        targetfaqsItem.style.maxHeight = `${targetfaqsItem.scrollHeight}px`;
+        targetfaqsItem.dataset.openfaqsList = true;
+        displayActiveIcon(
+          targetfaqsItem.querySelector(".minus-icon"),
+          targetfaqsItem.querySelector(".plus-icon")
+        );
+      } else {
+        closefaqsItems();
+      }
     }
-  } 
-} ); 
+  });
 
-faqList.addEventListener( "keydown", function( event ) {
-  if( event.target.closest( ".faq-item-header" ) && event.keyCode === 13 ) {
-    const faqItem = event.target.closest( ".faq-item" );
-    if( faqItem.dataset.openFaqList === "false" ) {
-      closeFaqItems();
-      faqItem.style.maxHeight = `${ faqItem.scrollHeight }px`;
-      faqItem.dataset.openFaqList = true;
-      displayActiveIcon(
-        faqItem.querySelector( ".minus-icon" ),
-        faqItem.querySelector( ".plus-icon" )
-      );
-    } else {
-      closeFaqItems();
+  // faqs Item Keydown Event
+  faqsList.addEventListener("keydown", (event) => {
+    const faqsItemHeader = event.target.closest(".faqs-item-header");
+    if (faqsItemHeader && event.keyCode === 13) {
+      const faqsItem = faqsItemHeader.closest(".faqs-item");
+      if (faqsItem.dataset.openfaqsList === "false") {
+        closefaqsItems();
+        faqsItem.style.maxHeight = `${faqsItem.scrollHeight}px`;
+        faqsItem.dataset.openfaqsList = true;
+        displayActiveIcon(
+          faqsItem.querySelector(".minus-icon"),
+          faqsItem.querySelector(".plus-icon")
+        );
+      } else {
+        closefaqsItems();
+      }
     }
-  }
-} );
+  });
 
-// Resize Observer
-let initialFaqCardWidth = faqCard.offsetWidth;
+  // Resize Observer
+  let initialfaqsCardWidth = faqsCard.offsetWidth;
 
-const resizeObserver = new ResizeObserver( entries => {
-  for( const entry of entries ) {
-    const { width } = entry.contentRect;
-    if( initialFaqCardWidth !== width ) {
-      faqItems.forEach( faqItem => {
-        if( faqItem.dataset.openFaqList === "false" ) {
-          const faqItemHeader = faqItem.querySelector( ".faq-item-header" );
-          faqItem.style.maxHeight = `${ faqItemHeader.offsetHeight }px`;
-        } else {
-          faqItem.style.maxHeight = `${ faqItem.scrollHeight }px`;
-        }
-      } );
+  const resizeObserver = new ResizeObserver((entries) => {
+    for (const entry of entries) {
+      const { width } = entry.contentRect;
+      if (initialfaqsCardWidth !== width) {
+        faqsItems.forEach((faqsItem) => {
+          const faqsItemHeader = faqsItem.querySelector(".faqs-item-header");
+          faqsItem.style.maxHeight =
+            faqsItem.dataset.openfaqsList === "false"
+              ? `${faqsItemHeader.offsetHeight}px`
+              : `${faqsItem.scrollHeight}px`;
+        });
+      }
     }
-  }
-} );
+  });
 
-resizeObserver.observe( faqCard );
+  resizeObserver.observe(faqsCard);
 
-} );
+});
